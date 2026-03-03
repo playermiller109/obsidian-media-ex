b871759eba005cb19af86fa130e3c6c3466fa282
playermiller109 <145541890+playermiller109@users.noreply.github.com>
2026-03-03 14:17:32 +0800

    refactor: link click handling to EditorView

--- app/src/patch/link.inline-field.ts
+++ app/src/patch/link.inline-field.ts
@@ -1,43 +1,36 @@
-import { around } from "monkey-around";
-import { MarkdownView } from "obsidian";
+import { EditorView } from "@codemirror/view";
 import { mediaSourceFields } from "@/media-note/note-index/def";
 import type MxPlugin from "@/mx-main";
 import { isModEvent } from "./mod-evt";
 
 export default function patchInlineUrl(this: MxPlugin) {
-  const clickHandler = (e: MouseEvent) => {
+  const clickHandler = (e: MouseEvent, cm: EditorView) => {
     const target = e.target as HTMLElement;
     if (!target.instanceOf(HTMLElement)) return;
-    if (
-      !target.matches(
-        ".metadata-property .metadata-property-value .external-link",
-      )
-    )
-      return;
-    const main = target.closest<HTMLElement>(".metadata-property");
-    if (!main) return;
-    const key = main.dataset.propertyKey;
-    if (!mediaSourceFields.includes(key as any)) return;
-    const urlInfo = this.resolveUrl(target.textContent);
-    if (!urlInfo) return;
-    e.stopImmediatePropagation();
-    this.leafOpener.openMedia(urlInfo, isModEvent(e), { fromUser: true });
-  };
-  const unload = around(MarkdownView.prototype, {
-    onload: (next) =>
-      function (this: MarkdownView) {
-        this.registerDomEvent(this.containerEl, "click", clickHandler, {
-          capture: true,
-        });
-        return next.call(this);
+
+    const linktext = target.textContent;
+    [
+      {
+        selector: ".metadata-property .metadata-property-value .external-link",
+        fn: () => {
+          const main = target.closest<HTMLElement>(".metadata-property");
+          if (!main) return;
+          const key = main.dataset.propertyKey;
+          if (!mediaSourceFields.includes(key as any)) return;
+          const urlInfo = this.resolveUrl(linktext);
+          if (!urlInfo) return;
+          e.stopImmediatePropagation();
+          this.leafOpener.openMedia(urlInfo, isModEvent(e), { fromUser: true });
+          return true;
+        }
       },
-  });
-  this.register(() => {
-    unload();
-    this.app.workspace
-      .getLeavesOfType("markdown")
-      .forEach((leaf) =>
-        leaf.view.containerEl.removeEventListener("click", clickHandler),
-      );
-  });
+    ]
+    .some(s => target.matches(s.selector) && s.fn());
+  }
+
+  this.registerEditorExtension([
+    EditorView.domEventHandlers({
+      click: clickHandler
+    })
+  ])
 }
