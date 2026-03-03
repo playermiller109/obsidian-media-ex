5bcb9a1940f22266de80785f736ccf87243ccbd6
playermiller109 <145541890+playermiller109@users.noreply.github.com>
2026-03-03 14:22:24 +0800

    feat: passThroughClick 1.12.4

--- app/src/patch/link.inline-field.ts
+++ app/src/patch/link.inline-field.ts
@@ -2,6 +2,8 @@ import { EditorView } from "@codemirror/view";
 import { mediaSourceFields } from "@/media-note/note-index/def";
 import type MxPlugin from "@/mx-main";
 import { isModEvent } from "./mod-evt";
+// since 1.12.4
+import * as utils from './link.inline-click';
 
 export default function patchInlineUrl(this: MxPlugin) {
   const clickHandler = (e: MouseEvent, cm: EditorView) => {
@@ -24,6 +26,17 @@ export default function patchInlineUrl(this: MxPlugin) {
           return true;
         }
       },
+      {
+        selector: ".cm-link > .cm-underline",
+        fn: () => {
+          const _ic = findLinkIcon(target.parentElement as Element);
+          if (!_ic) return;
+          e.stopImmediatePropagation();
+
+          utils.passThroughClick(e, cm);
+          return true;
+        }
+      },
     ]
     .some(s => target.matches(s.selector) && s.fn());
   }
@@ -33,4 +46,17 @@ export default function patchInlineUrl(this: MxPlugin) {
       click: clickHandler
     })
   ])
+}
+function findLinkIcon(start: Element): Element | null {
+  const parent = start.parentElement;
+  if (!parent) return null;
+  const kids = Array.from(parent.children);
+  const idx = kids.indexOf(start);
+  if (idx === -1) return null;
+  for (let i = idx + 1; i < kids.length; i++) {
+    const _ic = kids[i];
+    if (_ic.matches("span.cm-url.external-link")) return _ic;
+    if (_ic.matches("span.cm-link")) return null;
+  }
+  return null;
 }
